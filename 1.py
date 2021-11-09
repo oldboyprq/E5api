953f041c4a99024c3a8c4868b61822bf5f1c1df1
oldboyprq <67952647+oldboyprq@users.noreply.github.com>
2021-11-09 22:07:05 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,54 @@
+# -*- coding: UTF-8 -*-
+import requests as req
+import json,sys,time
+#先注册azure应用,确保应用有以下权限:
+#files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
+#user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
+#mail:  Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
+#注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
+
+
+
+
+
+
+path=sys.path[0]+r'/1.txt'
+
+def gettoken(refresh_token):
+    headers={'Content-Type':'application/x-www-form-urlencoded'
+            }
+    data={'grant_type': 'refresh_token',
+          'refresh_token': refresh_token,
+          'client_id':id,
+          'client_secret':secret,
+          'redirect_uri':'http://localhost:53682/'
+         }
+    html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
+    jsontxt = json.loads(html.text)
+    refresh_token = jsontxt['refresh_token']
+    access_token = jsontxt['access_token']
+    with open(path, 'w+') as f:
+        f.write(refresh_token)
+    return access_token
+def main():
+    fo = open(path, "r+")
+    refresh_token = fo.read()
+    fo.close()
+    localtime = time.asctime( time.localtime(time.time()) )
+    access_token=gettoken(refresh_token)
+    headers={
+    'Authorization':access_token,
+    'Content-Type':'application/json'
+    }
+    try:
+        r = req.get(r'https://graph.microsoft.com/v1.0/me/drive/sharedWithMe',headers=headers)
+        if r.status_code == 200:
+            print("与我共享的文件调用成功")
+        else:
+           print(r.status_code)
+        print('此次运行结束时间为 :', localtime)
+    except:
+        print("pass")
+        pass
+for _ in range(3):
+    main()
