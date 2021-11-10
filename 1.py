5505e5e64b2618c31b335aff832f079426f419c6
oldboyprq <67952647+oldboyprq@users.noreply.github.com>
2021-11-10 18:09:04 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -42,23 +42,23 @@ def main():
     }
     try:
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/sharedWithMe',headers=headers).status_code == 200:
-            print("与我共享的文件调用成功")
+            print("1、与我共享的文件调用成功")
         if req.get(r'https://graph.microsoft.com/v1.0/me/',headers=headers).status_code == 200:
-            print("我的个人资料调用成功")
+            print("2、我的个人资料调用成功")
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
-            print("我的邮件调用成功")
+            print("3、我的邮件调用成功")
         if req.get(r'https://graph.microsoft.com/v1.0/me/insights/trending',headers=headers).status_code == 200:
-            print("我的常用项目调用成功")
+            print("4、我的常用项目调用成功")
         if req.get(r'https://graph.microsoft.com/v1.0/me/calendars',headers=headers).status_code == 200:
-            print("我的日历调用成功")
+            print("5、我的日历调用成功")
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages?$search="hello world"',headers=headers).status_code == 200:
-            print("我的包含helloworld的邮件调用成功")
+            print("6、我的包含helloworld的邮件调用成功")
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
-            print("我的outlook类别调用成功")
+            print("7、我的outlook类别调用成功")
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/recent',headers=headers).status_code == 200:
-            print("我最近使用的文件调用成功")
+            print("8、我最近使用的文件调用成功")
         if req.get(r'https://graph.microsoft.com/v1.0/me/people',headers=headers).status_code == 200:
-            print("与我合作的人员调用成功")
+            print("9、与我合作的人员调用成功")
         print('此次运行结束时间为 :', localtime)
     except:
         print("pass")
